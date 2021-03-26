0b6524c5824659a1a0753820f0c3fa88211aba34
Rosty Semenyshyn <ripka.ua@gmail.com>
2021-03-26 15:30:47 +0100

    set up lib build

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+// hooks
+export { useCarouselTouch } from './hooks/useCarouselTouch';
+export { useCloseOnOuterClick } from './hooks/useCloseOnOuterClick';
+export { useEscPress } from './hooks/useEscPress';
+export { useScrolled } from './hooks/useScrolled';
+export { useWindowSize } from './hooks/useWindowSize';
+
+// functions
+export {
+	randomKey,
+	findGetParameter,
+	getWalk3dPreviewImg,
+	getVideoPreviewImg,
+	getVideoId,
+	randomLat,
+	randomLng,
+	randomCoords,
+	pageSize,
+	deviceType,
+	debounce,
+	getIcon,
+	getFeatureIcon,
+	featureName,
+	scrollTo,
+	hashCode,
+	isNumeric,
+	formatDate,
+	addCommas,
+	splitValue,
+	splitAddress,
+	splitText,
+	strToSlug,
+	replaceWithRandom,
+	textWidth,
+	hexToRgb,
+	iOS } from './helpers/functions'
+
+// framer
+export { collapseModal, fadeIn } from './helpers/framer'
+
+// story
+export { getSessionId, getData, setData } from './helpers/store'
+
+// constants
+export { constants } from './helpers/constants'
+
+
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js',
+		libraryTarget: 'umd'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env']
+					}
+				}
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					//'style-loader',
+					{
+						loader: 'css-loader',
+						options: { url: false, modules: true, importLoaders: 1 }
+					},
+					'sass-loader',
+				]
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg|css|eot|ttf)$/,
+				loader: 'url-loader',
+			}
+		]
+	},
+	externals: ['react', 'smoothscroll-polyfill', 'body-scroll-lock'],
+	mode: 'production'
+};
