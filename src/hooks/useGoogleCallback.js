d0e6fb9ddd1ff1d391524c05aa12c3c51e1ea7db
kachniss <vopalkova.katerina@gmail.com>
2021-03-30 15:21:32 -0700

    Update Google Places province to short

--- src/hooks/useGoogleCallback.js
+++ src/hooks/useGoogleCallback.js
@@ -16,7 +16,7 @@ export const useGoogleCallback = (fieldId, onChange) => {
             street_number: 'short_name',
             route: 'long_name',
             locality: 'long_name',
-            administrative_area_level_1: 'long_name',
+            administrative_area_level_1: 'short_name',
             country: 'long_name',
             postal_code: 'short_name'
         }
