9ea2bb625403295767291952fba82828407d7887
Rosty Semenyshyn <ripka.ua@gmail.com>
2021-04-14 21:07:32 +0200

    disabled encryption for testing

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,4 +1,4 @@
-import Cryptr from 'cryptr';
+//import Cryptr from 'cryptr';
 import Cookies from 'universal-cookie';
 import { hashCode } from './func_helpers';
 
@@ -29,13 +29,13 @@ export function setData(key, data) {
 	return res;
 }
 export function setUserData(data) {
-	const cryptr = new Cryptr(HASH_CODE);
-	const string = cryptr.encrypt(JSON.stringify(data));
-	return cookies.set('userData', string);
+	//const cryptr = new Cryptr(HASH_CODE);
+	//const string = cryptr.encrypt(JSON.stringify(data));
+	return cookies.set('userData', JSON.stringify(data));
 }
 export function getUserData() {
-	const cryptr = new Cryptr(HASH_CODE);
+	//const cryptr = new Cryptr(HASH_CODE);
 	let string = cookies.get('userData');
-	string = string ? cryptr.decrypt(string) : string;
-	return string && string.length && string !== 'undefined' ? JSON.parse(data) : null;
+	//string = string ? cryptr.decrypt(string) : string;
+	return string && string.length && string !== 'undefined' ? JSON.parse(string) : null;
 }
