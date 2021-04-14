acf0c7dcb4d04593631337286d073262bd43cabf
Rosty Semenyshyn <ripka.ua@gmail.com>
2021-04-14 20:56:15 +0200

    refactor of crypto use

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -3,7 +3,6 @@ import Cookies from 'universal-cookie';
 import { hashCode } from './func_helpers';
 
 const HASH_CODE = 'NU3NHexkAr6kEDxZahOcZ4sWGqiIinuT';
-const cryptr = new Cryptr(HASH_CODE);
 const cookies = new Cookies(HASH_CODE);
 
 export function getSessionId() {
@@ -30,10 +29,12 @@ export function setData(key, data) {
 	return res;
 }
 export function setUserData(data) {
+	const cryptr = new Cryptr(HASH_CODE);
 	const string = cryptr.encrypt(JSON.stringify(data));
 	return cookies.set('userData', string);
 }
 export function getUserData() {
+	const cryptr = new Cryptr(HASH_CODE);
 	let string = cookies.get('userData');
 	string = string ? cryptr.decrypt(string) : string;
 	return string && string.length && string !== 'undefined' ? JSON.parse(data) : null;
